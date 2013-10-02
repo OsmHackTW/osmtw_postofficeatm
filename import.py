800a81cbbe21d8f3a4f7c657fb84d97a296dde36
Rex Tsai <chihchun@kalug.linux.org.tw>
2013-10-02 23:37:30 +0800

    batch process.
    Signed-off-by: Rex Tsai <chihchun@kalug.linux.org.tw>

--- import.py
+++ import.py
@@ -5,6 +5,7 @@ import OsmApi
 import csv
 import sys
 from pprint import pprint
+from subprocess import call
 
 delta = 0.002
 readonly = True
@@ -12,30 +13,34 @@ debug = True
 
 osm = OsmApi.OsmApi(passwordfile="passwd", appid = 'RexBot', debug=debug, changesetauto=False)
 
+if (readonly is not True):
+    print ("changeset id %d" % (osm.ChangesetCreate({u"comment": u"中華郵政 ATM 資訊自動修正"})), file=sys.stderr)
+
 with open('post_atm_location.csv') as csvfile:
-     spamreader = csv.reader(csvfile, delimiter=',')
-     for row in spamreader:
-         match = list()
-         lon = float(row[6])
-         lat = float(row[7])
-         data = osm.Map(lon-delta, lat-delta, lon+delta, lat+delta);
-         for i in data:
-             if (i['data']['tag'] and 'amenity' in i['data']['tag'] and i['data']['tag']['amenity'] == 'atm'):
-                 match.append(i)
+    spamreader = csv.reader(csvfile, delimiter=',')
+    for row in spamreader:
+        match = list()
+        lon = float(row[6])
+        lat = float(row[7])
+        data = osm.Map(lon-delta, lat-delta, lon+delta, lat+delta);
+        for i in data:
+           if (i['data']['tag'] and 'amenity' in i['data']['tag'] and i['data']['tag']['amenity'] == 'atm'):
+                match.append(i)
 
-         if(len(match) == 1):
-             if (readonly is not True):
-                 print ("changeset id %d" % (osm.ChangesetCreate({u"comment": u"中華郵政 ATM 資訊自動修正"})), file=sys.stderr)
+        if(len(match) == 1):
+            node = match[0]['data']
+            node['tag']['operator'] = '中華郵政'
+            node['tag']['currency:twd'] = 'yes'
+            node['tag']['description'] = row[3]
+            node['tag']['description'] = "%s%s%s%s%s%s" % (row[3],row[12],row[8],row[9],row[10],row[11])
+            node['tag']['address'] = row[0]+row[1]+row[5]
+            node['tag']['phone'] = row[4].replace('(0', '+886-').replace(')','-').replace('轉', 'p')
+             
+            pprint(node)
+            # call(("/usr/bin/firefox http://www.openstreetmap.org/#map=19/%f/%f" % (lat, lon)).split())
+            if (not readonly):
+                osm.NodeUpdate(node)
 
-             node = match[0]['data']
-             node['tag']['operator'] = '中華郵政'
-             node['tag']['currency:twd'] = 'yes'
-             node['tag']['description'] = row[3]
-             node['tag']['description'] = "%s%s%s%s%s%s" % (row[3],row[12],row[8],row[9],row[10],row[11])
-             node['tag']['address'] = row[0]+row[1]+row[5]
-             node['tag']['phone'] = row[4].replace('(0', '+886-').replace(')','-').replace('轉', 'p')
+if (not readonly):
+    osm.ChangesetClose()
 
-             pprint(node)
-             if (not readonly):
-                 osm.NodeUpdate(node)
-                 osm.ChangesetClose()
